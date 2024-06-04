f6a34d7be58d51f2e9bb3b8f5bfd000dcc91b0cb
Aluno <Aluno@10.85.198.36>
2024-06-04 16:03:49 -0300

    commit project calculator04

---
+++ src/Calculator4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Aluno
+ */
+class Calculator4 {
+
+    void findTotal(int i, Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
